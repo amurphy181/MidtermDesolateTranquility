6ab2763c367242cdc5d3aa0dd7cc206f2f4896a1
Alex Murphy <alexander.f.murphy@gmail.com>
2018-06-20 16:58:41 -0600

    user DAO and Impl updated and stubbed

--- JPADesolateMidterm/src/com/skilldistillery/jpadesolatemidterm/data/UserDAO.java
+++ JPADesolateMidterm/src/com/skilldistillery/jpadesolatemidterm/data/UserDAO.java
@@ -1,5 +1,10 @@
 package com.skilldistillery.jpadesolatemidterm.data;
 
+import com.skilldistillery.jpadesolatemidterm.entities.User;
+
 public interface UserDAO {
+	
+	public User create(User user);
+	public User update(int id, User user);
 
 }
--- JPADesolateMidterm/src/com/skilldistillery/jpadesolatemidterm/data/UserDAOImpl.java
+++ JPADesolateMidterm/src/com/skilldistillery/jpadesolatemidterm/data/UserDAOImpl.java
@@ -1,5 +1,49 @@
 package com.skilldistillery.jpadesolatemidterm.data;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.skilldistillery.jpadesolatemidterm.entities.User;
+
 public class UserDAOImpl implements UserDAO {
+
+	@Override
+	public User create(User user) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("VideoStore");
+		EntityManager em = emf.createEntityManager();
+
+		// start the transaction
+		em.getTransaction().begin();
+		// write the customer to the database
+		em.persist(user);
+		// update the "local" Customer object
+		em.flush();
+		// commit the changes (actually perform the operation)
+		em.getTransaction().commit();
+//		em.close();
+		emf.close();
+		// return the Customer object
+		return user;
+	}
+
+	@Override
+	public User update(int id, User updatedUser) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("VideoStore");
+		EntityManager em = emf.createEntityManager();
+
+		em.getTransaction().begin();
+
+		User managed = em.find(User.class, id);
+//		managed.setFirstName(updatedUser.getFirstName());
+//		managed.setLastName(updatedUser.getLastName());
+
+		em.flush();
+		em.getTransaction().commit();
+
+//		em.close();
+		emf.close();
+		return managed;
+	}
 
 }
