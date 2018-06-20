af46582196c3a3701bc9707e1214d277deb70708
Joseph.Dibi <jjdibiasi13@gmail.com>
2018-06-20 16:57:04 -0600

    stubbed entities

--- JPADesolateMidterm/src/com/skilldistillery/jpadesolatemidterm/entities/Game.java
+++ JPADesolateMidterm/src/com/skilldistillery/jpadesolatemidterm/entities/Game.java
@@ -1,5 +1,19 @@
 package com.skilldistillery.jpadesolatemidterm.entities;
 
-public class Game {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Game {
+	
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Id
+	private int id;
+	private String title;
+	@Column(name="max_players")
+	private int maxPlayers;
+	
 }
--- JPADesolateMidterm/src/com/skilldistillery/jpadesolatemidterm/entities/Platform.java
+++ JPADesolateMidterm/src/com/skilldistillery/jpadesolatemidterm/entities/Platform.java
@@ -1,5 +1,16 @@
 package com.skilldistillery.jpadesolatemidterm.entities;
 
-public class Platform {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Platform {
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Id
+	private int id;
+	@Column(name="name")
+	private String platformName;
 }
