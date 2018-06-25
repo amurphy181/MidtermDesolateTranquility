e362e549e48feb5ad6238a203bf05a20b7f7dfb6
Joseph.Dibi <jjdibiasi13@gmail.com>
2018-06-25 09:25:13 -0600

    starting work on games

--- MVCDesolateMidterm/src/main/java/com/skilldistillery/mvcdesolatemidterm/data/GameDAO.java
+++ MVCDesolateMidterm/src/main/java/com/skilldistillery/mvcdesolatemidterm/data/GameDAO.java
@@ -1,5 +1,9 @@
 package com.skilldistillery.mvcdesolatemidterm.data;
 
+import com.skilldistillery.jpadesolatemidterm.entities.Game;
+
 public interface GameDAO {
+
+	Game addUserGame(int id, String game, String platform);
 
 }
