ac596b4a65159797b0b56b3349330b0765ec13cf
Joseph.Dibi <jjdibiasi13@gmail.com>
2018-06-26 13:58:16 -0600

    friends now entity table

---
+++ JPADesolateMidterm/src/com/skilldistillery/jpadesolatemidterm/entities/Friends.java
@@ -0,0 +1,102 @@
+package com.skilldistillery.jpadesolatemidterm.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Friends {
+
+	@Column(name="user_id")
+	@OneToOne
+	private User user;
+	@OneToOne
+	@Column(name="friend_id")
+	private User friend;
+	private boolean accepted;
+	private Date timestamp;
+	private String message;
+	
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public User getFriend() {
+		return friend;
+	}
+	public void setFriend(User friend) {
+		this.friend = friend;
+	}
+	public boolean isAccepted() {
+		return accepted;
+	}
+	public void setAccepted(boolean accepted) {
+		this.accepted = accepted;
+	}
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (accepted ? 1231 : 1237);
+		result = prime * result + ((friend == null) ? 0 : friend.hashCode());
+		result = prime * result + ((message == null) ? 0 : message.hashCode());
+		result = prime * result + ((timestamp == null) ? 0 : timestamp.hashCode());
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Friends other = (Friends) obj;
+		if (accepted != other.accepted)
+			return false;
+		if (friend == null) {
+			if (other.friend != null)
+				return false;
+		} else if (!friend.equals(other.friend))
+			return false;
+		if (message == null) {
+			if (other.message != null)
+				return false;
+		} else if (!message.equals(other.message))
+			return false;
+		if (timestamp == null) {
+			if (other.timestamp != null)
+				return false;
+		} else if (!timestamp.equals(other.timestamp))
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Friends [accepted=" + accepted + ", timestamp=" + timestamp + ", message=" + message + "]";
+	}
+	
+	
+}
