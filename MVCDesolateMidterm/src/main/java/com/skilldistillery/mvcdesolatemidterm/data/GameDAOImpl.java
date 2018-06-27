4d9efe9f01293efc144e24d05102db83252fa714
Joseph.Dibi <jjdibiasi13@gmail.com>
2018-06-27 11:01:56 -0600

    fixed friends that i broke somehow

--- MVCDesolateMidterm/src/main/java/com/skilldistillery/mvcdesolatemidterm/data/GameDAOImpl.java
+++ MVCDesolateMidterm/src/main/java/com/skilldistillery/mvcdesolatemidterm/data/GameDAOImpl.java
@@ -19,17 +19,16 @@ import com.skilldistillery.jpadesolatemidterm.entities.User;
 @Transactional
 @Component
 public class GameDAOImpl implements GameDAO {
-	
+
 	@Autowired
 	private EventDAO eventDao;
 	@Autowired
 	private GameDAO gameDao;
-	
+
 	@PersistenceContext
 	private EntityManager em;
-	
-	
-	//Users have a list of games, this allows users to add to the list.
+
+	// Users have a list of games, this allows users to add to the list.
 	@Override
 	public Game addUserGame(int id, String game, String platform) {
 		User userAddGame = em.find(User.class, id);
@@ -41,21 +40,25 @@ public class GameDAOImpl implements GameDAO {
 		userAddGame.addGame(addedGame);
 		em.flush();
 		return addedGame;
-		
+
 	}
-	
-	//If a user joins an event and the game is not in their library it will automatically be added.
+
+	// If a user joins an event and the game is not in their library it will
+	// automatically be added.
 	@Override
 	public boolean joinEventAddGame(int id, Game game) {
 		Game newGame = em.find(Game.class, game.getId());
 		boolean containGame = false;
 		User joinEventUser = em.find(User.class, id);
-			joinEventUser.addGame(newGame);
-			em.flush();
+		joinEventUser.addGame(newGame);
+		em.flush();
 		return containGame;
 	}
-	
-	//This long ass method removes the original game from the list of user games. Then the new game to be updated is checked against the database. if the exact game and platform combo exists that game is returned. Otherwise a new game is created and added to the db and user list
+
+	// This long ass method removes the original game from the list of user games.
+	// Then the new game to be updated is checked against the database. if the exact
+	// game and platform combo exists that game is returned. Otherwise a new game is
+	// created and added to the db and user list
 	@Override
 	public Game updateGame(int id, Game updatedGame, int userId) {
 		Game removedFromList = gameDao.findGameById(id);
@@ -64,56 +67,60 @@ public class GameDAOImpl implements GameDAO {
 		user.removeGame(removedFromList);
 		List<Game> allGames = gameDao.findAllGames();
 		for (Game game : allGames) {
-			if(updatedGame.getPlatform().getPlatformName().equals(game.getPlatform().getPlatformName()) && updatedGame.getTitle().equals(game.getTitle())) {
-			
+			if (updatedGame.getPlatform().getPlatformName().equals(game.getPlatform().getPlatformName())
+					&& updatedGame.getTitle().equals(game.getTitle())) {
+
 				managed = em.find(Game.class, game.getId());
 				user.addGame(managed);
 				return managed;
 			}
-			
+
 		}
 		if (managed == null) {
-			 managed = updatedGame;
+			managed = updatedGame;
 			managed = eventDao.createGame(managed);
-			
+
 		}
-		
-			em.flush();
-			user.addGame(managed);
+
+		em.flush();
+		user.addGame(managed);
 
 		return managed;
 	}
-	
-	//find specific game by id
+
+	// find specific game by id
 	@Override
 	public Game findGameById(int id) {
 		Game foundGame = em.find(Game.class, id);
-		
+
 		return foundGame;
 	}
-	//finds all games to display or find specific one from list
+
+	// finds all games to display or find specific one from list
 	@Override
 	public List<Game> findAllGames() {
 		String query = "select g from Game g";
 		List<Game> allGames = em.createQuery(query, Game.class).getResultList();
-		
-		
+
 		return allGames;
 	}
-//removes game from user list of games
+
+	// removes game from user list of games
 	@Override
 	public Game removeGame(int gameId, int userId) {
 		Game removedGame = em.find(Game.class, gameId);
 		User user = em.find(User.class, userId);
 		user.removeGame(removedGame);
-		
+
 		return removedGame;
 	}
-	
-//remember to move all functions below this line to user later
-//I want this function to allow users to add friends to their list. I don't want them to add themselves.
-//Do I want to view a list of all users to add or a search? lets have this just be a basic add.
-	//adds users to friends list
+
+	// remember to move all functions below this line to user later
+	// I want this function to allow users to add friends to their list. I don't
+	// want them to add themselves.
+	// Do I want to view a list of all users to add or a search? lets have this just
+	// be a basic add.
+	// adds users to friends list
 	@Override
 	public User addUserToFriendList(int userId, int friendId) {
 		User user = em.find(User.class, userId);
@@ -123,23 +130,29 @@ public class GameDAOImpl implements GameDAO {
 		em.flush();
 		return friend;
 	}
-	//removes
+
+	// removes
 	@Override
 	public User removeUserFromFriendList(int userId, int friendId) {
-		User user = em.find(User.class, userId);
+		String query = "select f from Friend f where f.user.id = :userId and f.friend.id= :friendId";
+		List<Friend> friendList = em.createQuery(query, Friend.class).setParameter("userId", userId)
+				.setParameter("friendId", friendId).getResultList();
+		for (Friend friend2 : friendList) {
+			friend2.setAccepted(false);
+		}
 		User friend = em.find(User.class, friendId);
-		user.removeFriend(friend);
 		return friend;
 	}
-	//shows all for adding users -- want to change to regex later
+
+	// shows all for adding users -- want to change to regex later
 	@Override
-	public List<User> showAllUsers(){
+	public List<User> showAllUsers() {
 		String query = "select u from User u";
 		List<User> allUsers = em.createQuery(query, User.class).getResultList();
 		return allUsers;
 	}
-	
-	//attempt to send friend request
+
+	// attempt to send friend request
 	@Override
 	public Friend sendFriendRequest(int userId, String message, int friendId) {
 		Friend request = new Friend();
@@ -153,38 +166,37 @@ public class GameDAOImpl implements GameDAO {
 		em.flush();
 		return request;
 	}
+
 	@Override
 	public Friend acceptFriendRequest(Friend friendRequest) {
 		Friend acceptRequest = em.find(Friend.class, friendRequest.getId());
 		acceptRequest.setAccepted(true);
-		gameDao.addUserToFriendList(acceptRequest.getFriend().getId(), acceptRequest.getUser().getId());
 		em.flush();
 		return acceptRequest;
 	}
+
 	@Override
 	public Friend findFriendRequest(int requestId) {
 		Friend request = em.find(Friend.class, requestId);
-		
+
 		return request;
 	}
-	
-	
-	
-	
-	
+
 	@Override
-	public List<User> findUserFriendList(int userId){
-		String query = "select f.friend.id from Friend f where (f.user.id = :id or f.friend.id = :id2) and f.accepted = 1";
-		List<Integer> friendIds = em.createQuery(query, Integer.class).setParameter("id", userId).setParameter("id2", userId).getResultList();
+	public List<User> findUserFriendList(int userId) {
+		String query = "select f from Friend f where (f.user.id = :id or f.friend.id = :id2) and f.accepted = 1";
+		List<Friend> friends = em.createQuery(query, Friend.class).setParameter("id", userId)
+				.setParameter("id2", userId).getResultList();
 		List<User> friendsList = new ArrayList<>();
-		for (Integer id : friendIds) {
-			friendsList.add(em.find(User.class, id));
-		}
-		if (friendsList.size() > 0) {
-		System.out.println(friendsList.get(0).getUserName());
+		for (Friend friend : friends) {
+			if (friend.getFriend().getId() != userId) {
+				friendsList.add(em.find(User.class, friend.getFriend().getId()));
+			}else if (userId != friend.getUser().getId()) {
+				friendsList.add(em.find(User.class, friend.getUser().getId()));
+			}
 		}
 		return friendsList;
-				
+
 	}
-	
+
 }
