db974ed069d48f4d9d5376e2d4c797ee57c64164
jmgroves <jared.m.groves@gmail.com>
2018-06-27 21:06:36 -0600

    cleaning up code

--- MVCDesolateMidterm/src/main/java/com/skilldistillery/mvcdesolatemidterm/controllers/UserController.java
+++ MVCDesolateMidterm/src/main/java/com/skilldistillery/mvcdesolatemidterm/controllers/UserController.java
@@ -5,33 +5,24 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.skilldistillery.jpadesolatemidterm.entities.Event;
 import com.skilldistillery.jpadesolatemidterm.entities.PasswordDTO;
 import com.skilldistillery.jpadesolatemidterm.entities.User;
-import com.skilldistillery.mvcdesolatemidterm.data.EventDAO;
-import com.skilldistillery.mvcdesolatemidterm.data.EventDAOImpl;
 import com.skilldistillery.mvcdesolatemidterm.data.UserDAO;
-import com.skilldistillery.mvcdesolatemidterm.data.UserDAOImpl;
 
 @Controller
 public class UserController {
 
 	@Autowired
 	private UserDAO userDao;
-	@Autowired
-	private EventDAO eventDAO;
-	
 
 	// you have the session to check for this
 	private boolean loggedIn;
@@ -44,7 +35,6 @@ public class UserController {
 		User user = new User();
 		mv.addObject("user", user);
 		mv.setViewName("WEB-INF/loginView.jsp");
-
 		return mv;
 	}
 
@@ -52,9 +42,7 @@ public class UserController {
 	public ModelAndView loginMethod(User user, HttpSession session, Errors error) {
 		ModelAndView mv = new ModelAndView();
 		User userLogin = userDao.findUserByUsername(user.getUserName());
-		System.out.println(userLogin);
 
-		System.out.println(user.getPassword());
 		if (userLogin != null) {
 			if (userDao.passwordConfirmation(userLogin, user.getPassword())) {
 
@@ -83,19 +71,12 @@ public class UserController {
 	public ModelAndView logoutMethod(@ModelAttribute("userCurrent") User user, HttpSession session,
 			RedirectAttributes flash) {
 		ModelAndView mv = new ModelAndView();
-
-		System.out.println(user);
 		User userLogout = (User) session.getAttribute("userCurrent");
-		System.out.println(userLogout);
-		System.out.println("!@!@!@!@ LOGOUT TESTER @!@!@!@!");
 
 		if (userLogout != null) {
 			loggedIn = false;
-			//session.invalidate();
 			flash.addFlashAttribute("logOut", userLogout.getUserName());
 			mv.setViewName("redirect:welcome.do");
-		} else {
-			System.out.println("LOGOUT failed.\n");
 		}
 
 		return mv;
@@ -108,9 +89,7 @@ public class UserController {
 		List<Event> completeEventList = userDao.listAllEvents();
 		mv.addObject("events", completeEventList);
 		mv.addObject("completeUserList", completeUserList);
-		System.out.println(completeUserList);
 		mv.setViewName("WEB-INF/adminPage.jsp");
-
 		return mv;
 	}
 
@@ -118,15 +97,8 @@ public class UserController {
 	public ModelAndView landingPageView(HttpSession session) {
 		ModelAndView mv = new ModelAndView();
 		List<Event> eventList = userDao.listAllEvents();
-
 		session.setAttribute("events", eventList);
 		User user = (User) session.getAttribute("userCurrent");
-		if (eventList == null) {
-			System.out.println("event list is null");
-		} else {
-			System.out.println("it went on through");
-		}
-		System.out.println(eventList);
 		session.setAttribute("userCurrent", user);
 		mv.setViewName("WEB-INF/landingPage.jsp");
 		return mv;
@@ -136,7 +108,6 @@ public class UserController {
 	public ModelAndView registerMethodView() {
 		ModelAndView mv = new ModelAndView();
 		User user = new User();
-
 		mv.addObject("user", user);
 		mv.setViewName("WEB-INF/registerView.jsp");
 		return mv;
@@ -146,11 +117,8 @@ public class UserController {
 	public ModelAndView Registered(User user, Errors error, RedirectAttributes flash, HttpSession session) {
 		ModelAndView mv = new ModelAndView();
 		if (userDao.uniqueUsername(user.getUserName())) {
-			// mv.addObject("user", user);
 			userDao.create(user);
-
 			boolean added = true;
-
 			flash.addFlashAttribute("added", added);
 			mv.setViewName("redirect:welcome.do");
 
@@ -166,7 +134,6 @@ public class UserController {
 	@RequestMapping(path = "deactivateUser.do", method = RequestMethod.POST)
 	public ModelAndView deactivateUser(HttpSession session, int id) {
 		ModelAndView mv = new ModelAndView();
-		System.out.println("**************" + id);
 		userDao.deactivateUser(id);
 		List<User> completeUserList = userDao.listAllUsers();
 		mv.addObject("completeUserList", completeUserList);
@@ -179,7 +146,6 @@ public class UserController {
 	@RequestMapping(path = "reactivateUser.do", method = RequestMethod.POST)
 	public ModelAndView reactivateUser(HttpSession session, int id) {
 		ModelAndView mv = new ModelAndView();
-		System.out.println("**************" + id);
 		userDao.reactivateUser(id);
 		List<User> completeUserList = userDao.listAllUsers();
 		mv.addObject("completeUserList", completeUserList);
@@ -192,7 +158,6 @@ public class UserController {
 	@RequestMapping(path = "deactivateAdmin.do", method = RequestMethod.POST)
 	public ModelAndView deactivateAdmin(HttpSession session, int id) {
 		ModelAndView mv = new ModelAndView();
-		System.out.println("************** " + id + "Deactivate admin status");
 		userDao.deactivateAdmin(id);
 		List<User> completeUserList = userDao.listAllUsers();
 		mv.addObject("completeUserList", completeUserList);
@@ -205,7 +170,6 @@ public class UserController {
 	@RequestMapping(path = "activateAdmin.do", method = RequestMethod.POST)
 	public ModelAndView activateAdmin(HttpSession session, int id) {
 		ModelAndView mv = new ModelAndView();
-		System.out.println("************** " + id + "Activate as Admin");
 		userDao.activateAdmin(id);
 		List<User> completeUserList = userDao.listAllUsers();
 		mv.addObject("completeUserList", completeUserList);
@@ -250,14 +214,12 @@ public class UserController {
 		mv.setViewName("redirect:profileView.do");
 		return mv;
 	}
-	
-	@RequestMapping(path="setProfilePicture.do", method = RequestMethod.POST)
+
+	@RequestMapping(path = "setProfilePicture.do", method = RequestMethod.POST)
 	public ModelAndView setProfilePicture(String picURL, int userId, HttpSession session, RedirectAttributes flash) {
 		ModelAndView mv = new ModelAndView();
-		System.out.println(userId);
-		if(userDao.setProfilePicture(userId, picURL)) {
+		if (userDao.setProfilePicture(userId, picURL)) {
 			boolean updated = true;
-			System.out.println("$$$ %% $$$ did we pass through?" );
 			flash.addFlashAttribute("Profile Picture Updated", updated);
 		} else {
 			boolean updated = false;
